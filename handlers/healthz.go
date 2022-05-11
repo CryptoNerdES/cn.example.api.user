b280414ccfde88c988ef11c019dc997a775aedb3
Sergio Herrero <sergioherre96@gmail.com>
2022-05-11 21:04:21 +0200

    Renamed packages

--- handlers/healthz.go
+++ handlers/healthz.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/CryptoNerdES/cn.example.api.user/models"
+	"github.com/CryptoNerdES/cn.example.api.user/models/responses"
 )
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.NewHealthzResponse())
+	json.NewEncoder(w).Encode(response.NewHealthzResponse())
 }
--- main_test.go
+++ main_test.go
@@ -7,26 +7,26 @@ import (
 	"testing"
 
 	"github.com/CryptoNerdES/cn.example.api.user/handlers"
-	"github.com/CryptoNerdES/cn.example.api.user/models"
+	response "github.com/CryptoNerdES/cn.example.api.user/models/responses"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReturnStatusOKWhenHealthz(t *testing.T) {
 	// Test set up
-	var response models.HealthzResponse
-	expectedResponse := models.NewHealthzResponse()
+	var res response.HealthzResponse
+	expectedResponse := response.NewHealthzResponse()
 	req := httptest.NewRequest("GET", "/healthz", nil)
 	w := httptest.NewRecorder()
 
 	// Execute the function to check
 	handlers.Healthz(w, req)
-	err := json.Unmarshal(w.Body.Bytes(), &response)
+	err := json.Unmarshal(w.Body.Bytes(), &res)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// Check if function is doing the work right
 	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
-	assert.Equal(t, expectedResponse.Message, response.Message)
-	assert.Equal(t, expectedResponse.StatusCode, response.StatusCode)
+	assert.Equal(t, expectedResponse.Message, res.Message)
+	assert.Equal(t, expectedResponse.StatusCode, res.StatusCode)
 }
